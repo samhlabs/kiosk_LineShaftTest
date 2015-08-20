98f000e8914838db23e34783634052e49d8fd99f
Phillip Elie <dddddeveloper@gmail.com>
2015-08-20 12:01:33 -0400

    Updating scripts

--- LineShaftKiosk/assets/js/script.js
+++ LineShaftKiosk/assets/js/script.js
@@ -72,20 +72,19 @@ jQuery(document).ready(function($)
 });
 
 $(function() {
+	TouchEmulator(); 
     FastClick.attach(document.body);
 });
 
-document.body.style.cursor = 'none';
 
+//$$('span', 'p', 'a');
 
-$$('span', 'p', 'a');
-
-$(window.document).on('mouseup touchend', function(event) {
+/*$(window.document).on('mouseup touchend', function(event) {
   // Capture this event anywhere in the document, since the mouse may leave our element while mouse is down and then the 'up' event will not fire within the element.
   mouseDown = false;
-});
+});*/
 
-$(document).ready(function(){
+/*$(document).ready(function(){
 	$("wrapper").css('cursor','url(assets/images/ic_panorama_fish_eye_white_24dp_2x.png),auto');
 	$("a").css('cursor','url(assets/images/ic_panorama_fish_eye_white_24dp_2x.png),auto');
-});
+});*/
